0f77f302406513312180d41bb7d9ce9379c9a877
fusobotic <fusionstudio3d@gmail.com>
2016-02-11 18:02:13 -0500

    Prototyping betting mechanic, started revolver

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coins : MonoBehaviour {
+
+	public float spinSpeed;
+
+	private Rigidbody rb;
+	private Quaternion startRot;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		startRot = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//print (rb.velocity);
+		//rb.AddTorque.transform.Rotate(rb.velocity);
+		//transform.Rotate(Mathf.Lerp(startRot, transform.rotation, Time.deltaTime));
+	}
+}
