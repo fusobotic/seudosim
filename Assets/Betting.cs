3a1ac81d94f2aa1f518965f26d199d1e42f70618
fusobotic <fusionstudio3d@gmail.com>
2016-02-12 00:05:06 -0500

    betting trigger finished, added All In functionality

---
+++ Assets/Betting.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Betting : MonoBehaviour {
+
+	public bool allIn;
+
+	private int coinCount;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		print (coinCount);
+
+		if (allIn) {
+			iTween.MoveUpdate (gameObject, iTween.Hash ("x", 0, "z", 0, "easeType", "easeInOutExpo", "time", 1f));
+		} else {
+			iTween.MoveUpdate (gameObject, iTween.Hash ("x", 6.46f, "z", 6.12f, "easeType", "easeInOutExpo", "time", 1f));
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Coin") {
+			coinCount++;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Coin") {
+			coinCount--;
+		}
+	}
+
+}
--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -7,17 +7,11 @@ public class Coins : MonoBehaviour {
 
 	private Rigidbody rb;
 	private Quaternion startRot;
+	private bool rotating = false;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
 		startRot = transform.rotation;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		//print (rb.velocity);
-		//rb.AddTorque.transform.Rotate(rb.velocity);
-		//transform.Rotate(Mathf.Lerp(startRot, transform.rotation, Time.deltaTime));
 	}
 }
