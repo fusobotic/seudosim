c2aa22b1deda8dd0e313a08576e6f6c358ea4c83
fusobotic <fusionstudio3d@gmail.com>
2016-03-29 15:57:46 -0400

    Added skeleton of item/shop system

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Items : MonoBehaviour {
+
+	public enum ItemType{Drink, Hat, Charm, Anim};
+
+	[System.Serializable]
+	public class Item {
+		public string name;
+		public GameObject model;
+		public bool bought;
+		public int price;
+		public string callName; //could do something different
+		public ItemType typeOf;
+
+		public void Buy(){
+			bought = true;
+		}
+
+	}
+
+	[System.Serializable]
+	public class Ammo {
+		public string name;
+		public GameObject model;
+		public int number;
+		public int price;
+		//don't need callname since it will be a bullet specific detection
+		//with a function directly on the bullet itself
+
+		public void Buy(){
+			number++;
+		}
+
+		public void Buy(int num){
+			number += num;
+		}
+
+		public void Expend(){
+			number--;
+		}
+	}
+
+	public Item[] items;
+	public Ammo[] ammos;
+
+	// Use this for initialization
+
+	void Equip (){
+
+
+
+
+	}
+
+	void Decriment (){
+
+	}
+}
