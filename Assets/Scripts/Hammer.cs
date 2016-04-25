fbe069aa7a6538dbd4af86e2a5a80c04f309c693
fusobotic <fusionstudio3d@gmail.com>
2016-04-25 01:27:27 -0400

    fixed new hammer rotation bug

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -5,10 +5,10 @@ public class Hammer : MonoBehaviour {
 
 	void OnMouseDown(){
 		print(transform.eulerAngles.x);
-		if (enabled && transform.eulerAngles.x == 0){
-			iTween.RotateTo(gameObject, iTween.Hash("x", 15, "easeType", "easeOutExpo", "time", .25));
+		if (enabled && transform.localEulerAngles.x == 0){
+			iTween.RotateTo(gameObject, iTween.Hash("x", 45, "easeType", "easeOutExpo", "islocal", true, "time", .25));
 		} else if (enabled){
-			iTween.RotateTo(gameObject, iTween.Hash("x", 90, "easeType", "easeInExpo", "time", .15));
+			iTween.RotateTo(gameObject, iTween.Hash("x", 0, "easeType", "easeInExpo", "islocal", true, "time", .15));
 		}
 	}
 }
